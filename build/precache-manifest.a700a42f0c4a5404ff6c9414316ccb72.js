a3078ab6697059133e2affd86029ff9e5691a109
Carlos <carlos.h.silva.vieira@gmail.com>
2018-12-05 22:40:31 -0200

    Build

---
+++ build/precache-manifest.a700a42f0c4a5404ff6c9414316ccb72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f0a326044ecf876b0abe",
+    "url": "/static/js/main.f0a32604.chunk.js"
+  },
+  {
+    "revision": "b9d70189a65dc8b242c4",
+    "url": "/static/js/1.b9d70189.chunk.js"
+  },
+  {
+    "revision": "f0a326044ecf876b0abe",
+    "url": "/static/css/main.25f742d9.chunk.css"
+  },
+  {
+    "revision": "ece6e365b46a04b178f1ff2aca5db567",
+    "url": "/index.html"
+  }
+];
